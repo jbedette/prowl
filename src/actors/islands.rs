1535762bb265f98bc825a40ac82470d95d497222
John Bedette <jbedette@yahoo.com>
2019-07-12 13:08:03 -0700

    island lyfe

---
+++ src/actors/islands.rs
@@ -0,0 +1,69 @@
+#[derive(Debug)]
+struct Pos {
+    id: u64,      //id of place
+    name: String, //name of place
+}
+#[derive(Debug)]
+struct Edge {
+    id: u64,    //id of edge,
+    dist: u32,  //weight of edge
+    start: Pos, //start pos of directed movement
+    end: Pos,
+}
+#[derive(Debug)]
+struct Island {
+    loc: Pos,            //name and id of island
+    pop: u64,            //population of island, affected by events
+    max_pop: u64,        //max pop is compared against pop for events, affected by events
+    targs: Vec<u64>,     //targs list of id's to set edges to
+    adj_list: Vec<Edge>, //edges are set after creation
+    res: Vec<String>,    //strings containing resource names, pobs good for enums
+}
+fn assign_edges(islands: &[Island]) {
+    let mut j: u64 = 0; //id for each edge, i think it's gonna be nice for record keeping
+    for i in islands {
+        for targ in i.targs {
+            i.adj_list.push(Edge {
+                id: j,
+                dist: 50,
+                start: Pos{ id: i.loc.id,  name:i.loc.name.to_string()},
+                //keep an eye on end, only works because of the way island ids are genereated
+                end: Pos{id: islands[targ as usize].loc.id, name: islands[targ as usize].loc.name.to_string()},
+            });
+            j += 1;
+        }
+    }
+}
+fn build() {
+    let isoles = vec![
+        "Albion".to_string(),
+        "Dreck".to_string(),
+        "Neboa".to_string(),
+        "Wenden".to_string(),
+    ];
+    let mut locs = Vec::new();
+    let mut targ_list = Vec::new();
+    let mut islands = Vec::new();
+    let mut j: u64 = 0; //note, fix later, figure out map
+    for i in &isoles {
+        locs.push(Pos {
+            id: j,
+            name: i.to_string(),
+        });
+        targ_list.push(j);
+        j += 1;
+    }
+    locs.reverse();
+    while !locs.is_empty() {
+        islands.push(Island {
+            loc: locs.pop().unwrap(),
+            pop: 50,
+            max_pop: 75,
+            targs: targ_list.clone(),
+            adj_list: Vec::new(),
+            res: vec!["iron".to_string(), "gold".to_string()],
+        });
+    }
+    assign_edges(&islands);
+    println!("{:?}", islands);
+}
