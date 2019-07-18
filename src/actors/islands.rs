c7ae65503fcb9509da2b729377a52ea939996a84
John Bedette <jbedette@yahoo.com>
2019-07-17 18:53:44 -0700

    mid work commit

--- src/actors/islands.rs
+++ src/actors/islands.rs
@@ -1,24 +1,41 @@
+use specs::{
+    world::Builder, Component, Read, ReadStorage, System, SystemData, VecStorage, World,
+    WriteStorage,
+};
+
 #[derive(Debug)]
-struct Pos {
-    id: u64,      //id of place
-    name: String, //name of place
+struct Pair {
+    i: u64,      //id
+    n: String, //name
 }
+impl Component for Pair{
+    type Storage = VecStorage<Self>;
+}
+
 #[derive(Debug)]
 struct Edge {
     id: u64,    //id of edge,
     dist: u32,  //weight of edge
-    start: Pos, //start pos of directed movement
-    end: Pos,
+    start: Pair, //start pos of directed movement
+    end: Pair,
 }
+impl Component for Edge{
+    type Storage = VecStorage<Self>;
+}
+
 #[derive(Debug)]
 struct Island {
-    loc: Pos,            //name and id of island
+    loc: Pair,            //name and id of island
     pop: u64,            //population of island, affected by events
     max_pop: u64,        //max pop is compared against pop for events, affected by events
     targs: Vec<u64>,     //targs list of id's to set edges to
     adj_list: Vec<Edge>, //edges are set after creation
     res: Vec<String>,    //strings containing resource names, pobs good for enums
 }
+impl Component for Island{
+    type Storage = VecStorage<Self>;
+}
+
 fn assign_edges(islands: &[Island]) {
     let mut j: u64 = 0; //id for each edge, i think it's gonna be nice for record keeping
     for i in islands {
@@ -26,7 +43,7 @@ fn assign_edges(islands: &[Island]) {
             i.adj_list.push(Edge {
                 id: j,
                 dist: 50,
-                start: Pos{ id: i.loc.id,  name:i.loc.name.to_string()},
+                start: Pair{ i: i.loc.id,  name:i.loc.name.to_string()},
                 //keep an eye on end, only works because of the way island ids are genereated
                 end: Pos{id: islands[targ as usize].loc.id, name: islands[targ as usize].loc.name.to_string()},
             });
@@ -34,7 +51,20 @@ fn assign_edges(islands: &[Island]) {
         }
     }
 }
-fn build() {
+fn main() {
+    let mut world = World::new();
+    world.add_enitity(Island);
+    world.add_entity(Edge);
+    world.add_entity(Pair);
+
+    let mut dispatcher = specs::DispatcherBuilder::new()
+        .with()
+        .with()
+    
+    dispatcher.dispatch(&mut world.res):
+    world.maintain();
+}
+fn build(){
     let isoles = vec![
         "Albion".to_string(),
         "Dreck".to_string(),
@@ -66,4 +96,5 @@ fn build() {
     }
     assign_edges(&islands);
     println!("{:?}", islands);
+    */
 }
